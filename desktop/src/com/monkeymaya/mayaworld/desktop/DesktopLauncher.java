c7614837357ac92b9007417e0e2c52999ba6be08
Ajay Bhaga <ajay.bhaga@gmail.com>
2017-08-25 07:22:48 -0400

    Added codebase.

---
+++ desktop/src/com/monkeymaya/mayaworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.monkeymaya.mayaworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.monkeymaya.mayaworld.MayaWorldGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MayaWorldGame(), config);
+	}
+}
